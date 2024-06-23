ec7a7ffafe437ac8ba8715f291162276edc77580
Karthik Saiharsh <137990475+KS-the-visionary@users.noreply.github.com>
2024-06-23 11:51:50 +0530

    Created logic.js

---
+++ views/home/logic.js
@@ -0,0 +1,2 @@
+document.querySelector(".titlebtn3").addEventListener('click', () => {window.brain.quitApp()});
+document.querySelector(".titlebtn2").addEventListener('click', () => {window.brain.minimiseApp()});
