6b703514ff2ea3de828195215dd099fa372d8d89
Karthik Saiharsh <137990475+KS-the-visionary@users.noreply.github.com>
2024-06-23 11:52:54 +0530

    Create logic.js

---
+++ views/obfusc/logic.js
@@ -0,0 +1,4 @@
+document.querySelector(".titlebtn3").addEventListener('click', () => {window.brain.quitApp()});
+document.querySelector(".titlebtn2").addEventListener('click', () => {window.brain.minimiseApp()});
+document.querySelector('.filePicker').addEventListener('click', () => {window.brain.obfuscateFilePath()});
+document.querySelector(".obfuscateFile").addEventListener('click', () => {window.brain.obfuscateFile()});
